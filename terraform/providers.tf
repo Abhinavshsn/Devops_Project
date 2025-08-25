2d6e2d6a4c7458a5f5f95916164fe06d98278fc6
Abhinavshsn <65085453+Abhinavshsn@users.noreply.github.com>
2025-08-26 00:04:35 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -18,21 +18,15 @@ terraform {
 provider "kind" {}
 
 provider "kubernetes" {
-  host                   = kind_cluster.default.endpoint
-  client_certificate     = kind_cluster.default.client_certificate
-  client_key             = kind_cluster.default.client_key
-  cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
+  config_path = ""            # leave blank
+  config_raw  = kind_cluster.default.kubeconfig_raw
 }
 
 provider "helm" {
   kubernetes {
-    host                   = kind_cluster.default.endpoint
-    client_certificate     = kind_cluster.default.client_certificate
-    client_key             = kind_cluster.default.client_key
-    cluster_ca_certificate = kind_cluster.default.cluster_ca_certificate
+    config_path = ""          # leave blank
+    config_raw  = kind_cluster.default.kubeconfig_raw
   }
 }
 
-#Terraform doesn’t allow using a resource (kind_cluster.default.kubeconfig_path) directly inside provider configuration, because providers are initialized before resources exist.
 
-#Fix: use the kubeconfig output from the kind_cluster resource instead. The kind_cluster resource gives you the raw kubeconfig as a string.SO don't use a path for kubeconfig(kubeconfig_path = pathexpand("/tmp/config")) to get the details here.
