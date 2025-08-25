38469320c3f575499d8382bb6f6b758f097e2c62
Abhinavshsn <65085453+Abhinavshsn@users.noreply.github.com>
2025-08-26 00:24:45 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -17,8 +17,8 @@ terraform {
 
 provider "kind" {}
 
-provider "kubernetes" {}
+provider "kubernetes" {config_path = "${path.module}/.kubeconfig.yaml"}
 
-provider "helm" {}
+provider "helm" {config_path = "${path.module}/.kubeconfig.yaml"}
 
 
