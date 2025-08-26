aa930de429b62a6b2c669913ac900630af044b6c
Abhinavshsn <abhinavshsn@gmail.com>
2025-08-26 10:00:48 +0530

    updated deploy.sh to fix loki and promtail mounts

--- Shell_scripts/deploy.sh
+++ Shell_scripts/deploy.sh
@@ -33,68 +33,76 @@ nodes:
 EOF
 
 echo "[INFO] Creating Kind cluster..."
-kind create cluster --name $CLUSTER_NAME --config $KIND_CONFIG_FILE
+kind create cluster --name $CLUSTER_NAME --config $KIND_CONFIG_FILE || echo "[INFO] Cluster may already exist"
 
 # CREATE NAMESPACES
 for ns in "${NAMESPACES[@]}"; do
     kubectl create namespace $ns || echo "[INFO] Namespace $ns already exists"
 done
 
-# INSTALL HELM CHARTS
+# HELM REPO SETUP
+helm repo add jenkins https://charts.jenkins.io
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm repo add linkerd https://helm.linkerd.io/stable
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
 echo "[INFO] Installing tools..."
 
 # CICD
-kubectl create namespace cicd || true
-helm repo add jenkins https://charts.jenkins.io
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
-helm install jenkins jenkins/jenkins --namespace cicd \
+helm upgrade --install jenkins jenkins/jenkins --namespace cicd \
     --set persistence.existingClaim="" \
     --set persistence.mountPath="$VOLUME_DIR/jenkins"
-helm install argo argo/argo-cd --namespace cicd \
+
+helm upgrade --install argo argo/argo-cd --namespace cicd \
     --set server.extraVolumeMounts[0].mountPath="$VOLUME_DIR/argocd" \
-    --set server.extraVolumeMounts[0].name="argocd-data"
+    --set server.extraVolumeMounts[0].name="argocd-data" \
+    --set server.extraVolumes[0].name="argocd-data" \
+    --set server.extraVolumes[0].hostPath.path="$VOLUME_DIR/argocd"
 
 # MONITORING
-kubectl create namespace monitoring || true
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-helm install prometheus prometheus-community/prometheus --namespace monitoring \
-    --set server.persistentVolume.existingClaim="" \
-    --set server.persistentVolume.mountPath="$VOLUME_DIR/prometheus"
-helm install grafana grafana/grafana --namespace monitoring \
-    --set persistence.existingClaim="" \
-    --set persistence.mountPath="$VOLUME_DIR/grafana"
-# Loki stack
-helm repo add grafana https://grafana.github.io/helm-charts
-helm install loki grafana/loki-stack --namespace monitoring \
-    --set persistence.enabled=true \
-    --set persistence.mountPath="$VOLUME_DIR/loki" \
-    --set promtail.enabled=true \
-    --set promtail.extraVolumeMounts[0].mountPath="$VOLUME_DIR/promtail"
+helm upgrade --install prometheus prometheus-community/prometheus --namespace monitoring \
+  --set server.persistentVolume.existingClaim="" \
+  --set server.persistentVolume.mountPath="$VOLUME_DIR/prometheus"
+
+helm upgrade --install grafana grafana/grafana --namespace monitoring \
+  --set persistence.existingClaim="" \
+  --set persistence.mountPath="$VOLUME_DIR/grafana"
+
+helm upgrade --install loki grafana/loki-stack --namespace monitoring \
+  --set persistence.enabled=true \
+  --set persistence.mountPath="$VOLUME_DIR/loki" \
+  --set promtail.enabled=true \
+  --set promtail.extraVolumes[0].name="promtail-data" \
+  --set promtail.extraVolumes[0].hostPath.path="$VOLUME_DIR/promtail" \
+  --set promtail.extraVolumeMounts[0].name="promtail-data" \
+  --set promtail.extraVolumeMounts[0].mountPath="$VOLUME_DIR/promtail"
 
 # SECURITY
-helm repo add kyverno https://kyverno.github.io/kyverno/
-helm install kyverno kyverno/kyverno --namespace security \
+helm upgrade --install kyverno kyverno/kyverno --namespace security \
     --set persistence.mountPath="$VOLUME_DIR/kyverno"
 
 # SERVICE MESH
-helm repo add linkerd https://helm.linkerd.io/stable
-helm install linkerd linkerd/linkerd2 --namespace service
+helm upgrade --install linkerd linkerd/linkerd2 --namespace service
 
-# <<< ADD THIS LINE IMMEDIATELY AFTER THE LINKERD INSTALL >>>
-kubectl label namespace application linkerd.io/inject=enabled
+# Enable Linkerd only in application namespace
+kubectl label namespace application linkerd.io/inject=enabled --overwrite
 
 # NETWORK
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm install nginx-ingress ingress-nginx/ingress-nginx --namespace network \
-    --set controller.extraVolumeMounts[0].mountPath="$VOLUME_DIR/nginx"
+helm upgrade --install nginx-ingress ingress-nginx/ingress-nginx --namespace network \
+    --set controller.extraVolumeMounts[0].mountPath="$VOLUME_DIR/nginx" \
+    --set controller.extraVolumeMounts[0].name="nginx-data" \
+    --set controller.extraVolumes[0].name="nginx-data" \
+    --set controller.extraVolumes[0].hostPath.path="$VOLUME_DIR/nginx"
 
 # INSPECTION
-kubectl create namespace inspection || true
 kubectl apply -f https://kubeshark.github.io/kubeshark/kubeshark.yaml -n inspection
 
+# APPLICATION
+kubectl apply -f $VOLUME_DIR/my-java-app/deployment.yaml -n application
 
 echo "[INFO] All tools deployed successfully!"
 echo "[INFO] KIND cluster is ready. Use 'kubectl get all -A' to see resources."
